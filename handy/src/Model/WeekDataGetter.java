04bcb997027f3bad9a16ec9d422ea07f6d83fce2
partner_jun <partner_jun@live.co.kr>
2016-01-20 14:38:33 +0900

    aa

--- handy/src/Model/WeekDataGetter.java
+++ handy/src/Model/WeekDataGetter.java
@@ -13,20 +13,27 @@ public class WeekDataGetter
   {
     DaoGetInfo daoGetInfo = new DaoGetInfo();
     UserWeekData[] data = new UserWeekData[7];
-    
+    float result = 0;
+	int sum=0;
+	int cnt=0;
 
     Calendar c = Calendar.getInstance();
     c.add(3, -1);
     
     int dayOfWeek = c.get(7);
+    
     c.add(5, (dayOfWeek - 1) * -1);
     
 
 
     int userSeq = daoGetInfo.getUser_seq(id);
+    
+    for(int i=0;i<7;i++)
+    	data[i] = new UserWeekData();
+    
     for (int i = 0; i < 7; i++)
     {
-      data[i] = new UserWeekData();
+      
       int year = c.get(1);
       int month = c.get(2) + 1;
       int day = c.get(5);
@@ -40,10 +47,29 @@ public class WeekDataGetter
       //TODO : user extra 데이터 입력받아와야함.
       //String gender, int age, int height, int weight, int avg_heart_rate
       	UserExtraBean ux = daoGetInfo.getExtraUser(userSeq);
-      // data[i].setConditionCalc(new SIHMConditionCalc(ux.getGender(), ux.getAge(), ux.getHeight(), ux.getWeight(), ux.getAvg_heart_rate()));
+          	
+      	// data[i].setConditionCalc(new SIHMConditionCalc(ux.getGender(), ux.getAge(), ux.getHeight(), ux.getWeight(), ux.getAvg_heart_rate()));
+    
+      	for(int j=0;j<data[i].getValueList().size();j++)
+		{
+			if(data[i].getValueList().get(j).steps<=75)
+			{
+				sum += data[i].getValueList().get(j).heart_rate;
+
+				//System.out.println(data[i].getValueList().get(j).log_date+ "ab" + data[i].getValueList().get(j).heart_rate);
+				cnt++;
+			}
+		}
+    
+    }
+    
+    result = sum/(float)cnt;
+    System.out.println("result: " + result );
+    System.out.println(cnt);
+    for(int i=0;i<data.length;i++)
+    {
       	
-      	
-      	data[i].setConditionCalc(new SIHMConditionCalc("M", 35, 180, 70, 75));
+      	data[i].setConditionCalc(new SIHMConditionCalc("M", 35, 180, 70, (int)result));
       	// FIXME 센싱데이터가 0개면 NaN 출력됨
       	if(data[i].getValueList().size() > 0)
       		data[i].getConditionCalc().calcPoints(data[i].getValueList());
