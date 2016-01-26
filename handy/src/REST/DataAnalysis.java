2a5d3da04aae2f976c19ec62449bed5342b6fa88
HYUNMIN-KIM <khyunm91@gmail.com>
2016-01-26 15:35:15 +0900

    리듬지수 수정

--- handy/src/REST/DataAnalysis.java
+++ handy/src/REST/DataAnalysis.java
@@ -50,30 +50,30 @@ public class DataAnalysis extends HttpServlet {
 			
 			String content = request.getParameter("content");
 			float point = Float.parseFloat(request.getParameter("point"));
-			int num=0;
+			int grade=0;
 			//ajax를 통해서 받아온 contents와 point를 처리
 			if(content.equalsIgnoreCase("ConditionDetail_"))
-				num = Condition_point(point);
+				grade = (int)point;
 			else if(content.equalsIgnoreCase("Temperature_"))
-				num = Temp_Hr_point(point);
+				grade = Temp_Hr_point(point);
 			else if(content.equalsIgnoreCase("TemperatureChange_"))
-				num = Sync_TempChange_point(point);
+				grade = Sync_TempChange_point(point);
 			else if(content.equalsIgnoreCase("TemperatureRhythm_"))
-				num = Rhythm_point(point);
+				grade = Rhythm_point(point);
 			else if(content.equalsIgnoreCase("HeartRate_"))
-				num = Temp_Hr_point(point);
+				grade = Temp_Hr_point(point);
 			else if(content.equalsIgnoreCase("HeartRateChange_"))
-				num = HrChange_point(point);
+				grade = HrChange_point(point);
 			else if(content.equalsIgnoreCase("HeartRateRhythm_"))
-				num = Rhythm_point(point);
+				grade = Rhythm_point(point);
 			else if(content.equalsIgnoreCase("Synchronization_"))
-				num = Sync_TempChange_point(point);
+				grade = Sync_TempChange_point(point);
 			else if(content.equalsIgnoreCase("Activity_"))
-				num = Activity_point(point);
+				grade = Activity_point(point);
 
 			response.setContentType("text/plain");
 				
-				out.println(Data(content,num));
+				out.println(Data(content,grade));
 			
 		}catch(Exception e){
 			e.printStackTrace();
@@ -94,13 +94,13 @@ public class DataAnalysis extends HttpServlet {
 
 	}
 	//위키피디아 parsing
-	public String Data(String sub, int num)
+	public String Data(String sub, int grade)
 	{
 
 		String anal = "";
 		Document doc = null;
 		try {
-			doc = Jsoup.connect("http://123.212.190.35/tiki-index_raw.php?page="+sub+num+"&full&format=jqs5").timeout(0).get();
+			doc = Jsoup.connect("http://123.212.190.35/tiki-index_raw.php?page="+sub+grade+"&full&format=jqs5").timeout(0).get();
 		} catch (IOException e) {
 			System.out.println("error");
 		}
@@ -118,104 +118,104 @@ public class DataAnalysis extends HttpServlet {
     //컨디션 점수 등급
 	public int Condition_point(float con_p)
 	{
-		int num=1;
+		int grade=1;
 
 		if(con_p>=90)
-			num=10;
+			grade=10;
 		else if(con_p<90 && con_p>=80)
-			num=9;
+			grade=9;
 		else if(con_p<80 && con_p>=70)
-			num=8;
+			grade=8;
 		else if(con_p<70 && con_p>=60)
-			num=7;
+			grade=7;
 		else if(con_p<60 && con_p>=50)
-			num=6;
+			grade=6;
 		else if(con_p<50 && con_p>=40)
-			num=5;
+			grade=5;
 		else if(con_p<40 && con_p>=30)
-			num=4;
+			grade=4;
 		else if(con_p<30 && con_p>=20)
-			num=3;
+			grade=3;
 		else if(con_p<20 && con_p>=10)
-			num=2;
+			grade=2;
 		else if(con_p<10)
-			num=1;
+			grade=1;
 
-		return num;
+		return grade;
 
 	}
 	//온도와 맥박 점수
 	public int Temp_Hr_point(float point)
 	{
-		int num=0;
+		int grade=0;
 		if(point>=90)
-			num=1;
+			grade=1;
 		else if(point<90 && point>=80)
-			num=2;
+			grade=2;
 		else if(point<80 && point>=70)
-			num=3;
+			grade=3;
 		else if(point<70)
-			num=4;
+			grade=4;
 
-		return num;
+		return grade;
 	}
 	//싱크로와 온도 변화 점수 등급
 	public int Sync_TempChange_point(float point)
 	{
-		int num=0;
+		int grade=0;
 
 		if(point>=15)
-			num=1;
+			grade=1;
 		else if(point<15 && point>=10)
-			num=2;
+			grade=2;
 		else if(point<10 && point>=5)
-			num=3;
+			grade=3;
 		else if(point<5)
-			num=4;
+			grade=4;
 
-		return num;
+		return grade;
 	}
 	//맥박변동 점수 등급
 	public int HrChange_point(float point)
 	{
-		int num=0;
+		int grade=0;
 
 		if(point>=20)
-			num=1;
+			grade=1;
 		else if(point<20 && point>=15)
-			num=2;
+			grade=2;
 		else if(point<15 && point>=10)
-			num=3;
+			grade=3;
 		else if(point<10)
-			num=4;
+			grade=4;
 
-		return num;
+		return grade;
 	}
 	//리듬 점수 등급
 	public int Rhythm_point(float point)
 	{
-		int num=0;
+		int grade=0;
 
-		if(point>0)
-			num=2;
-		else
-			num=1;
-		return num;
+		if(point>=0.0)
+			grade=2;
+		else if(point<0)
+			grade=1;
+		return grade;
 	}
 	//운동량 점수 등급
 	public int Activity_point(float point)
 	{
-		int num=0;
+		int grade=0;
 		if(point>=100)
-			num=1;
+			grade=1;
 		else if(point<100 && point>=80)
-			num=2;
+			grade=2;
 		else if(point<80 && point>=60)
-			num=3;
+			grade=3;
 		else if(point<60)
-			num=4;
+			grade=4;
 
-		return num;
+		return grade;
 	}
 	
 }
