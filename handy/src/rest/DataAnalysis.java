ec10d800bd1283a6baa3ceed35424df325686f4b
HYUNMIN-KIM <khyunm91@gmail.com>
2016-01-29 17:03:39 +0900

    점수 등급 class in util

--- handy/src/rest/DataAnalysis.java
+++ handy/src/rest/DataAnalysis.java
@@ -12,6 +12,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import util.ActivityFormat;
+import util.HeartChangeDeductFormat;
+import util.HeartTemperatureFormat;
+import util.RhythmFormat;
+import util.SyncTempChangeDeductFormat;
+
 
 /**
  * Servlet implementation class DataAnalysis
@@ -47,21 +53,21 @@ public class DataAnalysis extends HttpServlet {
 			if(content.equalsIgnoreCase("ConditionDetail_"))
 				grade = (int)point;
 			else if(content.equalsIgnoreCase("Temperature_"))
-				grade = Temp_Hr_point(point);
+				grade = HeartTemperatureFormat.format(point);
 			else if(content.equalsIgnoreCase("TemperatureChange_"))
-				grade = Sync_TempChange_point(point);
+				grade = SyncTempChangeDeductFormat.format(point);
 			else if(content.equalsIgnoreCase("TemperatureRhythm_"))
-				grade = Rhythm_point(point);
+				grade = RhythmFormat.format(point);
 			else if(content.equalsIgnoreCase("HeartRate_"))
-				grade = Temp_Hr_point(point);
+				grade = HeartTemperatureFormat.format(point);
 			else if(content.equalsIgnoreCase("HeartRateChange_"))
-				grade = HrChange_point(point);
+				grade = HeartChangeDeductFormat.format(point);
 			else if(content.equalsIgnoreCase("HeartRateRhythm_"))
-				grade = Rhythm_point(point);
+				grade = RhythmFormat.format(point);
 			else if(content.equalsIgnoreCase("Synchronization_"))
-				grade = Sync_TempChange_point(point);
+				grade = SyncTempChangeDeductFormat.format(point);
 			else if(content.equalsIgnoreCase("Activity_"))
-				grade = Activity_point(point);
+				grade = ActivityFormat.format(point);
 
 			response.setContentType("text/plain");
 				
@@ -107,107 +113,5 @@ public class DataAnalysis extends HttpServlet {
 
 		return anal;
 	}
-    //컨디션 점수 등급
-	public int Condition_point(float con_p)
-	{
-		int grade=1;
-
-		if(con_p>=90)
-			grade=10;
-		else if(con_p<90 && con_p>=80)
-			grade=9;
-		else if(con_p<80 && con_p>=70)
-			grade=8;
-		else if(con_p<70 && con_p>=60)
-			grade=7;
-		else if(con_p<60 && con_p>=50)
-			grade=6;
-		else if(con_p<50 && con_p>=40)
-			grade=5;
-		else if(con_p<40 && con_p>=30)
-			grade=4;
-		else if(con_p<30 && con_p>=20)
-			grade=3;
-		else if(con_p<20 && con_p>=10)
-			grade=2;
-		else if(con_p<10)
-			grade=1;
-
-		return grade;
-
-	}
-	//온도와 맥박 점수
-	public int Temp_Hr_point(float point)
-	{
-		int grade=0;
-		if(point>=90)
-			grade=1;
-		else if(point<90 && point>=80)
-			grade=2;
-		else if(point<80 && point>=70)
-			grade=3;
-		else if(point<70)
-			grade=4;
-
-		return grade;
-	}
-	//싱크로와 온도 변화 점수 등급
-	public int Sync_TempChange_point(float point)
-	{
-		int grade=0;
-
-		if(point>=15)
-			grade=1;
-		else if(point<15 && point>=10)
-			grade=2;
-		else if(point<10 && point>=5)
-			grade=3;
-		else if(point<5)
-			grade=4;
-
-		return grade;
-	}
-	//맥박변동 점수 등급
-	public int HrChange_point(float point)
-	{
-		int grade=0;
-
-		if(point>=20)
-			grade=1;
-		else if(point<20 && point>=15)
-			grade=2;
-		else if(point<15 && point>=10)
-			grade=3;
-		else if(point<10)
-			grade=4;
-
-		return grade;
-	}
-	//리듬 점수 등급
-	public int Rhythm_point(float point)
-	{
-		int grade=0;
-
-		if(point>=0.0)
-			grade=2;
-		else if(point<0)
-			grade=1;
-		return grade;
-	}
-	//운동량 점수 등급
-	public int Activity_point(float point)
-	{
-		int grade=0;
-		if(point>=100)
-			grade=1;
-		else if(point<100 && point>=80)
-			grade=2;
-		else if(point<80 && point>=60)
-			grade=3;
-		else if(point<60)
-			grade=4;
-
-		return grade;
-	}
-	
+    
 }
